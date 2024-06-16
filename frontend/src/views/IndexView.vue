f559e970d891c4546e779decb4d1bd0882dbfb50
timmn <timmmmnn@gmail.com>
2024-06-16 19:14:57 +0200

    Added Voice Recording

--- frontend/src/views/IndexView.vue
+++ frontend/src/views/IndexView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Chat from "../components/Chat.vue";
+import Voicechat from "../components/Voicechat.vue";
 </script>
 
 <template>
    <Chat />
+   <Voicechat />   
 </template>
 
 <style scoped lang="scss"></style>
